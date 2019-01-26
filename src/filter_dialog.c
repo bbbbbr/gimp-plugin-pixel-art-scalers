8e4c35715a66e8f1376f29d6da42280139e936cd
birch <bbbbbr@users.noreply.github.com>
2019-01-25 17:29:05 -0800

    Initial algorithm selection handling, some cleanup

--- src/filter_dialog.c
+++ src/filter_dialog.c
@@ -17,7 +17,38 @@ extern const char PLUG_IN_PROCEDURE[];
 extern const char PLUG_IN_ROLE[];
 extern const char PLUG_IN_BINARY[];
 
-static void filter_apply (uint32_t *, uint32_t *, int, int, int);
+static void filter_apply (int, uint32_t *, uint32_t *, int, int);
+static void copy_scaled_to_unscaled(guchar *, guchar *, guint, guint, guint, guint);
+
+enum scaler_list {
+    SCALER_HQ2X,
+    SCALER_HQ3X,
+    SCALER_HQ4X,
+    SCALER_ENUM_LAST
+};
+
+typedef struct {
+    void (*scaler_function)(uint32_t*, uint32_t*, int, int);
+    int  scale_factor;
+} scaler_info;
+
+scaler_info scalers[SCALER_ENUM_LAST];
+
+
+void scalers_init() {
+
+    // Init HQX scaler library
+    hqxInit();
+
+    scalers[SCALER_HQ2X].scaler_function = &hq2x_32;
+    scalers[SCALER_HQ2X].scale_factor    = 2;
+
+    scalers[SCALER_HQ3X].scaler_function = &hq3x_32;
+    scalers[SCALER_HQ3X].scale_factor    = 3;
+
+    scalers[SCALER_HQ4X].scaler_function = &hq4x_32;
+    scalers[SCALER_HQ4X].scale_factor    = 4;
+ }
 
 /*******************************************************/
 /*                    Dialog                           */
@@ -159,18 +190,16 @@ void pixel_art_scalers_run (GimpDrawable *drawable, GimpPreview  *preview)
     GimpPixelRgn src_rgn, dest_rgn;
     gint         bpp, has_alpha;
     gint         width, height;
-    gint         progress, max_progress;
 
-    guchar       * src_row,  * s;
-    guchar       * dest_row, * d;
     gint         x, y;
     gint         alpha;
-    gpointer     pixel_tile_sub_region;
 
-    uint32_t     * p_workbuf = NULL;
-    uint32_t     * p_outputbuf = NULL;
-    guchar       * p_pix = NULL;
+    uint32_t     * p_srcbuf = NULL;
+    uint32_t     * p_scaledbuf = NULL;
 
+
+    // Initialize the scalers
+    scalers_init();
 
     // Get the working image area for either the preview sub-window or the entire image
     if (preview) {
@@ -188,7 +217,7 @@ void pixel_art_scalers_run (GimpDrawable *drawable, GimpPreview  *preview)
     alpha = (has_alpha) ? drawable->bpp - 1 : drawable->bpp;
 
     // Allocate a working buffer to copy the source image into
-    p_workbuf = (uint32_t *) g_new (guchar, width * height * bpp);
+    p_srcbuf = (uint32_t *) g_new (guchar, width * height * bpp);
 
     // FALSE, FALSE : region will be used to read the actual drawable datas
     // Initialize source pixel region with drawable
@@ -199,6 +228,9 @@ void pixel_art_scalers_run (GimpDrawable *drawable, GimpPreview  *preview)
                          FALSE, FALSE);
 
     if (!preview) {
+
+        // TODO: Fix assumption that image has alpha layer (BytesPerPixel = 4)
+
         // TRUE,  TRUE  : region will be used to write to the shadow tiles
         //                i.e. make changes that will be written back to source tiles
         // Initialize dest pixel region with drawable
@@ -212,87 +244,49 @@ void pixel_art_scalers_run (GimpDrawable *drawable, GimpPreview  *preview)
 
     // Copy source image to working buffer
     gimp_pixel_rgn_get_rect (&src_rgn,
-                              (guchar *) p_workbuf,
-                              x, y, width, height);
+                             (guchar *) p_srcbuf,
+                             x, y, width, height);
 
 
 
-/// ---------
+    // BEGIN SCALER
 
-    guint scale_factor = 4;
+    guint scaler_mode = SCALER_HQ4X;
+
+    guint scale_factor = scalers[scaler_mode].scale_factor;
 
     // Allocate output buffer for the results
     // guchar   : unsigned char ( 8 bits)
     // guint32  : unsigned int  (32 bits)
     // uint32_t : unsigned int  (32 bits)
-    // p_outputbuf = (uint32_t *) malloc(width * scale_factor * height * scale_factor * sizeof(uint32_t));
-    p_outputbuf = (uint32_t *) g_new (guint, width * scale_factor * height * scale_factor * sizeof(uint32_t));
+    p_scaledbuf = (uint32_t *) g_new (guint, width * scale_factor * height * scale_factor * sizeof(uint32_t));
 
-    if (p_outputbuf) {
+    if (p_scaledbuf) {
 
-        // TODO: Careful! Making assumptions about p_workbuf : bpp = 4 / uint32_t here
-        filter_apply (p_workbuf,
-                      p_outputbuf,
-                      (int) width,
-                      (int) height,
-                      (int) scale_factor);
+        // TODO: Careful! Making assumptions about p_srcbuf : bpp = 4 / uint32_t here
+        filter_apply (scaler_mode,
+                      p_srcbuf,
+                      p_scaledbuf,
+                      (int) width, (int) height);
 
 
-         // Copy the output back on top of the working/preview buffer
-        //   This only copies a windowed subset of the output (size matching input buffer pre-scaled)
-        guchar       * p_w = (guchar *) p_workbuf;
-        guchar       * p_o = (guchar *) p_outputbuf;
-        for (gint y = 0; y < height; y++) {
-                   // TODO: fix assumptions about bpp/etc here - NOTE note using BPP, since increment is in uint32_ts
-                   memcpy(p_w, p_o, width * bpp);
-                   p_w +=  width * bpp;                // increment row for dest buffer
-                   p_o +=  width * scale_factor * bpp; // increment row for source buffer
-                  // if (has_alpha) //
-        }
+        copy_scaled_to_unscaled((guchar *) p_srcbuf,
+                                (guchar *)p_scaledbuf,
+                                width, height,
+                                bpp, scale_factor);
 
-
-        // memcpy(p_workbuf, p_outputbuf, width * height * bpp);
-
-
-
-
-        g_free(p_outputbuf);
+        g_free(p_scaledbuf);
     }
-
-/*
-    // Use a temp pointer to operate on the working buffer
-    p_pix = (guchar *) p_workbuf;
-
-    // Iterate over the working buffer and modify it
-    for (gint y = 0; y < height; y++) {
-        for (gint x = 0; x < width; x++) {
-              *(p_pix++) ^= 0xFF; //[0]
-              *(p_pix++) ^= 0xFF; //[1]
-              *(p_pix++) ^= 0xFF; //[2]
-
-              if (has_alpha) //
-                  p_pix++;
-        }
-
-        // Only update the progress bar status if it's not a preview
-        if (!preview) {
-            gimp_progress_update ((double) y / (double) height);
-        }
-    }
-*/
 
     // Filter is done, apply the update
     if (preview) {
-        gimp_preview_draw_buffer (preview, (guchar *) p_workbuf, width * bpp);
+        gimp_preview_draw_buffer (preview, (guchar *) p_srcbuf, width * bpp);
     }
     else
     {
-        // Update progress to 100% complete
-        gimp_progress_update (1.0);
-
         // Copy working buffer to the shadow image
         gimp_pixel_rgn_set_rect (&dest_rgn,
-                                  (guchar *) p_workbuf,
+                                  (guchar *) p_srcbuf,
                                   x, y, width, height);
 
         // Apply the changes to the image (merge shadow, update drawable)
@@ -302,18 +296,35 @@ void pixel_art_scalers_run (GimpDrawable *drawable, GimpPreview  *preview)
     }
 
     // Free the working buffer
-    g_free (p_workbuf);
+    g_free (p_srcbuf);
 }
 
 
+static void copy_scaled_to_unscaled(guchar * p_srcbuf, guchar * p_scaledbuf, guint width, guint height, guint bpp, guint scale_factor) {
 
-static void filter_apply (uint32_t * p_srcbuf, uint32_t * p_destbuf, int width, int height, int scale_factor) {
+    // Copy the output back on top of the working/preview buffer
+    //   This only copies a windowed subset of the output (size matching input buffer pre-scaled)
+    for (gint y = 0; y < height; y++) {
 
-    hqxInit();
+         // TODO: fix assumptions about bpp/etc here - NOTE note using BPP, since increment is in uint32_ts
+         // Because I can never remember:  memcopy direction (p1, <-- p2)
+         memcpy(p_srcbuf, p_scaledbuf, width * bpp);
+         p_srcbuf +=  width * bpp;                // increment row for dest buffer
+         p_scaledbuf +=  width * scale_factor * bpp; // increment row for source buffer
 
-    hq4x_32( (uint32_t*) p_srcbuf,
-             (uint32_t*) p_destbuf,
-                   (int) width,
-                  (int) height);
+        // TODO: if (has_alpha)
+    }
 }
 
+
+static void filter_apply(int scaler_mode, uint32_t * p_srcbuf, uint32_t * p_destbuf, int width, int height) {
+
+    if (scaler_mode < SCALER_ENUM_LAST) {
+
+        // Call the requested scaler function
+        scalers[scaler_mode].scaler_function( (uint32_t*) p_srcbuf,
+                                              (uint32_t*) p_destbuf,
+                                                    (int) width,
+                                                    (int) height);
+    }
+}
